a1a12969597a5e9bd94b5102a4a2e79aad626df6
lukeasmi <lukeasmi@cisco.com>
2022-07-25 19:39:26 +0200

    Cleaning

--- src/pages/exercise-analysis.js
+++ src/pages/exercise-analysis.js
@@ -1,102 +1,100 @@
-import * as React from "react"
-import { useState, useEffect } from "react"
-import Dropdown from "react-bootstrap/Dropdown"
-import DropdownButton from "react-bootstrap/DropdownButton"
-import Col from "react-bootstrap/Col"
-import Form from "react-bootstrap/Form"
+import * as React from "react";
+import { useState } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
-import FunctionLayout from "../components/function-layout"
-import Seo from "../components/seo"
-import { ChartLayout } from "../components/charts"
-
-import { updatePlayedNotes, updateExerciseAnalysisData, extractExerciseAnalysisData } from "../midi-analysis/midi-analysis"
-
+import FunctionLayout from "../components/function-layout";
+import Seo from "../components/seo";
+import { ChartLayout } from "../components/charts";
+import { updatePlayedNotes, updateExerciseAnalysisData, extractExerciseAnalysisData } from "../midi-analysis/midi-analysis";
 import { midiInput } from './index';
 
 
 function getChartData(exerciseData, graphs) {
-  let titles = ["Velocity On", "Velocity Off", "Durations", "Intervals"]
-  let data = [exerciseData.velocityOn, exerciseData.velocityOff, exerciseData.durations, exerciseData.intervals]
-  let chartData = [[],[]]
+  const titles = ["Velocity On", "Velocity Off", "Durations", "Intervals"];
+  const data = [exerciseData.velocityOn, exerciseData.velocityOff, exerciseData.durations, exerciseData.intervals];
+  const chartData = [[],[]];
   for ( let i=0; i<graphs.length; i++ ) {
     if ( graphs[i] ) {
-      chartData[0].push(data[i])
-      chartData[1].push(titles[i])
+      chartData[0].push(data[i]);
+      chartData[1].push(titles[i]);
     }
   }
-  return chartData
+  return chartData;
 }
 
 const Exercise_Analysis = () => {
-  let [played, setPlayed] = useState([])
-  let [graphs, setGraphs] = useState([true,true,true,true])
-  let initExerciseData = {
+  const [played, setPlayed] = useState([]);
+  const [graphs, setGraphs] = useState([true,true,true,true]);
+  const initExerciseData = {
     "velocityOn":   [],
     "velocityOff":  [],
     "intervals":    [],
     "durations":    [],
-  }
-  let [exerciseData, setExerciseData] = useState(initExerciseData)
+  };
+  const [exerciseData, setExerciseData] = useState(initExerciseData);
+  const options = [{id:'0',name:'Velocity On',selected:true},{id:'1',name:'Velocity Off',selected:true},{id:'2',name:'Duration',selected:true},{id:'3',name:'Intervals',selected:true}];
 
   const onSelectedOptionsChange = (e) => {
-    let newGraphs = [...graphs]
-    newGraphs[e.target.id] = !newGraphs[e.target.id]
-    setGraphs(newGraphs)
+    const newGraphs = [...graphs];
+    newGraphs[e.target.id] = !newGraphs[e.target.id];
+    setGraphs(newGraphs);
   }
 
   midiInput.onmidimessage = ( message ) => {
-    let playedNew = updatePlayedNotes(message,played)
-    // let exerciseDataNew = updateExerciseAnalysisData(exerciseData,playedNew) // TODO: this is not efficient, should simply update the existing array if slow
+    let playedNew = updatePlayedNotes(message,played);
+    // const exerciseDataNew = updateExerciseAnalysisData(exerciseData,playedNew) // TODO: this is not efficient, should simply update the existing array if slow
     let exerciseDataNew = extractExerciseAnalysisData(playedNew) // TODO: this is not efficient, should simply update the existing array if slow
     if ( exerciseDataNew.intervals.length > 1 ) {
       for (let i=exerciseDataNew.intervals.length-1; i>=0; i--) {
         if ( exerciseDataNew.intervals[i].value > 1000 ) { // TODO: let the user decide the interval
-          playedNew = updatePlayedNotes(message,[])
+          playedNew = updatePlayedNotes(message,[]);
           // exerciseDataNew = updateExerciseAnalysisData(initExerciseData,playedNew)
-          exerciseDataNew = extractExerciseAnalysisData(playedNew)
-          break
+          exerciseDataNew = extractExerciseAnalysisData(playedNew);
+          break;
         }
       }
     }
-    setPlayed(playedNew)
-    setExerciseData(exerciseDataNew)
+    setExerciseData(exerciseDataNew);
+    setPlayed(playedNew);
+    console.log('Here');
   }
 
-  // const [options, setOptions] = useState([{id:0,name:'Velocity On',selected:true},{id:1,name:'Velocity Off',selected:true},{id:2,name:'Duration',selected:true},{id:3,name:'Intervals',selected:true}])
-  const options = [{id:'0',name:'Velocity On',selected:true},{id:'1',name:'Velocity Off',selected:true},{id:'2',name:'Duration',selected:true},{id:'3',name:'Intervals',selected:true}]
 
   return (
-  <FunctionLayout title="Exercise Analysis">
-    {/* <Multiselect 
-      // data={multiSelectState} 
-      data={[{value:'0',label:'Velocity On',selected:true},{value:'1',label:'Velocity Off',selected:true},{value:'2',label:'Duration',selected:true},{value:'3',label:'Intervals',selected:true}]}
-      onChange={handleChange}  
-      multiple
-      buttonText={function(options, select) {return "Graphs"}} 
-    /> */}
-    <DropdownButton
-    alignRight
-    title="Graphs"
-    id="dropdown-menu-align-right"
-    // onSelect={handleSelect}
-    >
-      {options.map(options => (
-        <Dropdown.ItemText>
-          <Form.Check
-              type="checkbox"
-              id={options.id}
-              name={options.name}
-              label={options.name}
-              defaultChecked={options.selected}
-              onChange={onSelectedOptionsChange}
-          />
-        </Dropdown.ItemText>
-      ))}
-    </DropdownButton>
+    <FunctionLayout title="Exercise Analysis">
+      {/* <Multiselect 
+        // data={multiSelectState} 
+        data={[{value:'0',label:'Velocity On',selected:true},{value:'1',label:'Velocity Off',selected:true},{value:'2',label:'Duration',selected:true},{value:'3',label:'Intervals',selected:true}]}
+        onChange={handleChange}  
+        multiple
+        buttonText={function(options, select) {return "Graphs"}} 
+      /> */}
+      <DropdownButton
+      alignright="true"
+      title="Graphs"
+      id="dropdown-menu-align-right"
+      // onSelect={handleSelect}
+      >
+        {options.map(options => (
+          <Dropdown.ItemText key={options.id}>
+            <Form.Check
+                type="checkbox"
+                id={options.id}
+                name={options.name}
+                label={options.name}
+                defaultChecked={options.selected}
+                onChange={onSelectedOptionsChange}
+            />
+          </Dropdown.ItemText>
+        ))}
+      </DropdownButton>
 
-    <ChartLayout chartData={getChartData(exerciseData, graphs)}></ChartLayout>
-  </FunctionLayout>
-  )
+      <ChartLayout chartData={getChartData(exerciseData, graphs)}></ChartLayout>
+    </FunctionLayout>
+  );
 }
 
-export default Exercise_Analysis
+export default Exercise_Analysis;
