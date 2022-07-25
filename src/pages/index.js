e11acc8908f246bbff24d473898f9c5a89fcf531
lukeasmi <lukeasmi@cisco.com>
2022-07-25 19:39:34 +0200

    Cleaning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { useState } from "react"
-import { Link, navigate } from "gatsby"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import ButtonGroup from "react-bootstrap/ButtonGroup"
+import React from "react";
+import { useState } from "react";
+import { Link, navigate } from "gatsby";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 // TODO: Global variables are bad, but I failed to pass through state parameter in link so works for now
-export let midiInput = null
+export let midiInput = null;
 
 
 function onMIDIFailure(msg) {
@@ -18,48 +18,48 @@ function onMIDIFailure(msg) {
 
 function getMidiDevices( midiAccess ) {
   if ( midiAccess === "Loading" ) {
-    return "No midi inputs"
+    return "No midi inputs";
   } else {
-    let inputs = []
+    let inputs = [];
     for (let entry of midiAccess.inputs) {
       let input = entry[1];
-      inputs.push({"name": input.name, "id": input.id})
-      console.log(input.id)
+      inputs.push({"name": input.name, "id": input.id});
+      console.log(input.id);
     }
-    return inputs
+    return inputs;
   }
 }
 
 const IndexPage = () => {
-  const [midiAccess, setMidiAccess] = useState(null)
-  const [devices, setDevices] = useState([])
-  const [inputChosen, setInputChosen] = useState(false)
+  const [midiAccess, setMidiAccess] = useState(null);
+  const [devices, setDevices] = useState([]);
+  const [inputChosen, setInputChosen] = useState(false);
 
   navigator.requestMIDIAccess().then( ( access ) => {
     if ( midiAccess == null ) {
-      setMidiAccess(access)
-      setDevices(getMidiDevices( access ))
+      setMidiAccess(access);
+      setDevices(getMidiDevices( access ));
     }
-  }, onMIDIFailure)
+  }, onMIDIFailure);
 
   function updateMidiDevices() {
-    setDevices(getMidiDevices( midiAccess ))
+    setDevices(getMidiDevices( midiAccess ));
   }
 
   const selectMidiDevice = (e) => {
-    let id = e.target.value
-    console.log(id)
+    let id = e.target.value;
+    console.log(id);
     if ( midiAccess.inputs.size === 0 ) {
-      console.log("No MIDI Input")
+      console.log("No MIDI Input");
     } else {
-      let newInput = midiAccess.inputs.get(id)
-      midiInput = newInput // TODO: Don't use global variables
-      setInputChosen(true)
+      let newInput = midiAccess.inputs.get(id);
+      midiInput = newInput; // TODO: Don't use global variables
+      setInputChosen(true);
     }
   }
   
   return (
-    <div class="home-flex">
+    <div className="home-flex">
       <h1>Piano Analysis</h1>
       <p>10x your playing</p>
       <p>Select your midi device</p>
@@ -69,7 +69,10 @@ const IndexPage = () => {
         onChange={selectMidiDevice}>
         <option>Select Midi Device</option>
         {devices.map( device => (
-            <option value={device.id}>{device.name}</option>
+            <option value={device.id}
+                    key={device.name}>
+                      {device.name}
+            </option>
         ))}
       </Form.Select>
       {inputChosen ? (
@@ -77,6 +80,7 @@ const IndexPage = () => {
         <p>And choose what to analyse</p>
         <ButtonGroup aria-label="Basic example" vertical>
           <Button variant="outline-primary" onClick={() => navigate("/exercise-analysis")}>Exercises</Button>
+          <Button variant="outline-primary" onClick={() => navigate("/polyrythm")}>Polyrythm</Button>
           <Button variant="outline-primary" onClick={() => navigate("/")}>(Pedaling)</Button>
           <Button variant="outline-primary" onClick={() => navigate("/")}>(Chords)</Button>
           <Button variant="outline-primary" onClick={() => navigate("/")}>(Pieces)</Button>
@@ -87,10 +91,10 @@ const IndexPage = () => {
       )
       }
     </div>
-  )
+  );
 }
 
-export default IndexPage
+export default IndexPage;
 
   /*
   TODO: 
