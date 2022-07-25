293f311c27d0351f594e17ed40e6222a5a4b6282
lukeasmi <lukeasmi@cisco.com>
2022-07-25 19:39:01 +0200

    Use the react bootstrap navbar

--- src/components/header.js
+++ src/components/header.js
@@ -2,27 +2,32 @@ import * as React from "react"
 import Dropdown from "react-bootstrap/Dropdown"
 import { Link } from "gatsby"
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const Header = ({ title }) => {
     return (
-        <header class="page-header">
-        <nav>
-            <h2 class="logo">
-            {title}
-            </h2>
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Function
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/exercise-analysis">Exercise Analysis</Link></Dropdown.Item>
-                    <Dropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">(Pedal Analysis)</Link></Dropdown.Item>
-                    <Dropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">(Chord Analysis)</Link></Dropdown.Item>
-                    <Dropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">(Piece Analysis)</Link></Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        </nav>
-        </header>
-    )
+        <Navbar bg="light" expand="lg" style={{ maxHeight: '64px' }} >
+          <Container>
+            <Navbar.Brand href="#home">{title}</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#link">Help</Nav.Link>
+                <Nav.Link href="#link">Midi Device</Nav.Link>
+                <NavDropdown title="Navigation" id="basic-nav-dropdown">
+                    <NavDropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/exercise-analysis">Exercise Analysis</Link></NavDropdown.Item>
+                    <NavDropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">(Pedal Analysis)</Link></NavDropdown.Item>
+                    <NavDropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">(Chord Analysis)</Link></NavDropdown.Item>
+                    <NavDropdown.Item><Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">(Piece Analysis)</Link></NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
 }
 
 export default Header
